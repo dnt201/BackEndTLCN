fd130e81981ee4862e0b403d065e820200c8ee04
Duc Minh Quan <bignumd@gmail.com>
2022-09-18 17:26:55 +0700

    Update User Infomation
    - Update User Entity
    - Update Permission For Update User Information
    - Add 2 New API for Update User Information

--- src/modules/users/controllers/user.controller.ts
+++ src/modules/users/controllers/user.controller.ts
@@ -1,9 +1,13 @@
+import { Permission } from './../entities/permission.entity';
 import {
+  Body,
   ClassSerializerInterceptor,
   Controller,
   Get,
   NotFoundException,
   Param,
+  Put,
+  Req,
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
@@ -12,6 +16,10 @@ import {
 import { UsersService } from '../services/users.service';
 import { User_Permission as ListPermission } from '../permission/permission';
 import { PermissionGuard } from 'src/auth/guards/permission.guard';
+import { UpdateUserDTO } from '../dtos/updateUser.dto';
+import RequestWithUser from 'src/auth/interfaces/requestWithUser.interface';
+import JwtAuthenticationGuard from 'src/auth/guards/jwt-authentication.guard';
+import { use } from 'passport';
 
 @Controller('users')
 @UseInterceptors(ClassSerializerInterceptor)
@@ -23,6 +31,26 @@ export class UsersController {
   @UseGuards(PermissionGuard(ListPermission.ViewAllUser))
   async getAllUsers() {
     return await this.usersService.getAllUsers();
+  }
+
+  @Put('/admin/edit/:id')
+  @UseGuards(PermissionGuard(ListPermission.UpdateUserInfo))
+  async updateUserInfoWithAdminRole(
+    @Param() { id },
+    @Body() updateUserData: UpdateUserDTO,
+  ) {
+    const user = await this.usersService.getUserById(id);
+    if (!user) throw new NotFoundException(`Not found user with id: ${id}`);
+    return await this.usersService.updateUserInfoWithAdmin(id, updateUserData);
+  }
+
+  @Put('/edit')
+  @UseGuards(JwtAuthenticationGuard)
+  async updateUserInfo(@Req() request, @Body() userInfo: UpdateUserDTO) {
+    const user = request.user;
+    user.role = undefined;
+    user.permission = undefined;
+    return await this.usersService.updateUserInfo(user, userInfo);
   }
 
   @Get('/deleted')
--- src/modules/users/dtos/updateUser.dto.ts
+++ src/modules/users/dtos/updateUser.dto.ts
@@ -1,6 +1,9 @@
+import { Gender } from 'src/common/constants/gender.constant';
+
 export class UpdateUserDTO {
   username: string;
   email: string;
   phoneNumber: string;
   shortInfo: string;
+  gender: Gender;
 }
