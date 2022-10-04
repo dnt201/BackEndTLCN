51df606cd8b47c1791735167ab45a361e80f1a20
Quản Minh Đức <58112725+KaiKhongNgu@users.noreply.github.com>
2022-10-04 22:29:50 +0700

    Update Upload File for Create Post
    Update Upload File for Create Post

--- src/modules/files/controllers/file.controller.ts
+++ src/modules/files/controllers/file.controller.ts
@@ -6,16 +6,49 @@ import {
   ClassSerializerInterceptor,
   StreamableFile,
   Res,
+  Post,
+  UseGuards,
+  PayloadTooLargeException,
+  Req,
+  UploadedFile,
 } from '@nestjs/common';
 import { Response } from 'express';
 import { createReadStream } from 'fs';
 import { join } from 'path';
 import { FileService } from '../services/file.service';
+import { FilesInterceptor } from 'src/modules/files/interceptors/file.interceptor';
+import JwtAuthenticationGuard from 'src/auth/guards/jwt-authentication.guard';
+import RequestWithUser from 'src/auth/interfaces/requestWithUser.interface';
 
 @Controller('file')
 @UseInterceptors(ClassSerializerInterceptor)
 export default class FileController {
   constructor(private readonly fileService: FileService) {}
+
+  @Post('create')
+  @UseGuards(JwtAuthenticationGuard)
+  @UseInterceptors(
+    FilesInterceptor({
+      fieldName: 'file',
+      path: '/files',
+      fileFilter: (request, file, callback) => {
+        callback(null, true);
+      },
+      limits: {
+        fileSize: Math.pow(1024, 2) * 50, // 50MB
+      },
+    }),
+  )
+  async addFile(
+    @Req() request: RequestWithUser,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    return this.fileService.saveLocalFileData({
+      path: file.path,
+      filename: file.originalname,
+      mimetype: file.mimetype,
+    });
+  }
 
   @Get(':id')
   async getDatabaseFileById(
