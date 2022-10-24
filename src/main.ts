c80af0bfff50c3a8588215cd0d9a05d62bd68b8d
Quản Minh Đức <58112725+KaiKhongNgu@users.noreply.github.com>
2022-10-24 13:38:51 +0700

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const port = process.env.PORT;
 
   app.useGlobalPipes(new ValidationPipe());
