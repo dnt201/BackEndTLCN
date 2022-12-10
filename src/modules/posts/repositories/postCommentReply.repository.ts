f7ccff912f8067affbee7c14e213f60dee93915b
Quản Minh Đức <58112725+KaiKhongNgu@users.noreply.github.com>
2022-12-10 19:10:35 +0700

    Delete Reply and Comment

--- src/modules/posts/repositories/postCommentReply.repository.ts
+++ src/modules/posts/repositories/postCommentReply.repository.ts
@@ -1,4 +1,9 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpStatus,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { PostReplyDTO } from '../dtos/createReply.dto';
 import { PostReply } from '../entities/postReply.entity';
@@ -24,6 +29,20 @@ export class PostReplyRepository extends Repository<PostReply> {
       return await this.getReplyById(id);
     } catch (error) {
       throw new InternalServerErrorException(error.message);
+    }
+  }
+
+  async deleteReply(id: string) {
+    try {
+      const deletedResponse = await this.delete(id);
+      if (!deletedResponse.affected) {
+        throw new BadRequestException(`Post Reply does not exist`);
+      }
+      return true;
+    } catch (error) {
+      if (error.code === HttpStatus.BAD_REQUEST)
+        throw new BadRequestException(error.message);
+      else throw new InternalServerErrorException(error.message);
     }
   }
 
